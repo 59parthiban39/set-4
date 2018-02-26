74ebe5b42fb238592394b4486c9393bd79dc508f
59parthiban39 <35764937+59parthiban39@users.noreply.github.com>
2018-02-26 15:53:36 +0530

    Create s41\26\02.C

---
+++ s41\26\02.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char X[100];
+	
+	int i,count=0,Y;
+	
+	scanf("%s",&X);
+	Y=strlen(X);
+	for(i=0;i<Y;i++)
+	{
+	
+		if((X[i]='a')&&(X[i]='z')||(X[i]='A')&&(X[i]='Z'))
+		{
+		
+			count++;
+		}
+	}
+	
+	printf("%d",count);
+	return 0; 
+}
